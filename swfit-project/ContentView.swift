97d2664df35eded90536c0fa9a6a19ff261dd16c
zhangzhongyou <zhangzhongyou@bytedance.com>
2024-08-23 17:52:03 +0800

    Initial Commit

---
+++ swfit-project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swfit-project
+//
+//  Created by ByteDance on 2024/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swfit-project/swfit_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  swfit_projectApp.swift
+//  swfit-project
+//
+//  Created by ByteDance on 2024/8/23.
+//
+
+import SwiftUI
+
+@main
+struct swfit_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
